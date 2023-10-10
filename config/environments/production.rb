035e602342ffc79fc6976461ea2c6604f76df116
crokibolt <manuelmmendoza4@gmail.com>
2023-10-10 18:02:27 -0300

    idk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
