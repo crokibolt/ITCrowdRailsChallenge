c6ebb54561ec59f149d5a32c2def3e713203e6cd
crokibolt <manuelmmendoza4@gmail.com>
2023-10-10 17:40:26 -0300

    master key config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Enable static file serving from the `/public` folder (turn off if using NGINX/Apache for it).
   config.public_file_server.enabled = true
@@ -86,8 +86,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
